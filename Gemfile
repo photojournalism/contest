e00b5f3194e9ca7e16d80283ec2689e359c1e964
Casey Scarborough <caseyscarborough@gmail.com>
2014-08-07 10:24:52 -0400

    Add RSpec.

--- Gemfile
+++ Gemfile
@@ -45,3 +45,17 @@ gem 'spring',        group: :development
 
 # Devise for authentication
 gem 'devise'
+
+gem 'biggs'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+end
+
+group :test do
+  gem 'faker'
+  gem 'capybara'
+  gem 'guard-rspec'
+  gem 'launchy'
+end
--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,17 @@ module Contest
       authentication:       EMAIL_CONFIG['authentication'],
       enable_starttls_auto: EMAIL_CONFIG['enable_starttls_auto']
     }
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => false,
+        :helper_specs => false,
+        :routing_specs => false,
+        :controller_specs => true,
+        :request_specs => true
+
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
