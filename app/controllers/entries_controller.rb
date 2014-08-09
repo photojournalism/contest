6e5354a5cc52397fd624505034d329ed547c650a
Casey Scarborough <caseyscarborough@gmail.com>
2014-08-09 16:09:15 -0400

    Allow contest entry only when contest is open.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,16 @@
 class EntriesController < ApplicationController
 
+  before_action :authenticate_user!
+
   def new
-    @categories = Category.all.order(:name)
+    @contest = Contest.current
+    if !@contest.has_started?
+      flash.now[:notice] = t('contest.not_open', :contest => @contest, :open_date => @contest.formatted_open_date).html_safe
+      return
+    elsif @contest.has_ended?
+      flash.now[:notice] = t('contest.ended', :contest => @contest).html_safe
+      return
+    end
+    @categories = @contest.categories
   end
 end
--- app/models/contest.rb
+++ app/models/contest.rb
@@ -8,4 +8,44 @@ class Contest < ActiveRecord::Base
       errors.add(:close_date, "Close date must be after open date") if close_date < open_date
     end
   end
+
+  def is_open?
+    now = Date.new
+    if open_date < now && close_date > now
+      return true
+    end
+    return false
+  end
+
+  def has_started?
+    now = Date.new
+    if open_date < now
+      return true
+    end
+    return false
+  end
+
+  def has_ended?
+    now = Date.new
+    if close_date < now
+      return true
+    end
+    return false
+  end
+
+  def formatted_open_date
+    open_date.strftime("%A, %b. %-d, %Y at %-I:%M%P")
+  end
+
+  def formatted_close_date
+    close_date.strftime("%A, %b. %-d, %Y at %-I:%M%P")
+  end
+
+  def self.current
+    Contest.all.order('year DESC').first
+  end
+
+  def to_s
+    "#{year} #{name}"
+  end
 end
