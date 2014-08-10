276d1f27bd6811daafbde74bfebf1f22291b2924
Casey Scarborough <caseyscarborough@gmail.com>
2014-08-10 17:28:41 -0400

    Add jQuery fileupload assets and move app-related assets to separate directory.

---
+++ app/assets/javascripts/jquery-fileupload.js
@@ -0,0 +1,9 @@
+//= require jquery-fileupload/jquery.ui.widget.js
+//= require jquery-fileupload/jquery.iframe-transport.js
+//= require jquery-fileupload/jquery.fileupload.js
+//= require jquery-fileupload/jquery.fileupload-process.js
+//= require jquery-fileupload/jquery.fileupload-image.js
+//= require jquery-fileupload/jquery.fileupload-video.js
+//= require jquery-fileupload/jquery.fileupload-validate.js
+//= require jquery-fileupload/jquery.fileupload-ui.js
+//= require jquery-fileupload/main.js
