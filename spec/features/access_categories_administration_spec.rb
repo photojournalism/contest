d4f14ab4fded3e29e4e9d87bad44fcfc9cc6082b
Casey Scarborough <caseyscarborough@gmail.com>
2014-08-10 15:17:14 -0400

    Add feature specs for admin section.

---
+++ spec/features/access_categories_administration_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+feature 'access categories administration' do
+
+  let(:user) { FactoryGirl.create(:user) }
+  let(:category) { FactoryGirl.create(:category) }
+
+  describe 'as not logged in user' do
+    scenario 'visit categories url' do
+      visit categories_path
+      expect(page).to have_content('Please log in first')
+    end
+  end
+
+  describe 'as non-admin user' do
+    scenario 'visit categories url' do
+      user.admin = false
+      user.save
+      sign_in(user.email, user.password)
+      visit categories_path
+      expect(page).to have_content('not authorized')
+    end
+  end
+
+  describe 'as admin user' do
+    before(:each) do
+      user.admin = true
+      user.save
+
+      sign_in(user.email, user.password)
+    end
+
+    scenario 'visit categories url' do
+      category.save
+      visit categories_path
+      expect(page).to have_content(category.name)
+    end
+  end
+
+  def sign_in(email, password)
+    visit new_user_session_path
+
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_button 'Sign in'
+  end
+end
---
+++ spec/features/access_user_administration_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+feature 'access user administration' do
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe 'as not logged in user' do
+    scenario 'visit users url' do
+      sign_out user
+      visit users_path
+      expect(page).to have_content('Please log in first')
+    end
+
+    scenario 'visit users profile' do
+      sign_out user
+      visit user_path(:id => user.id)
+      expect(page).to have_content('Please log in first')
+    end
+  end
+
+  describe 'as non-admin user' do
+    before(:each) do
+      sign_in(user.email, user.password)
+    end
+
+    scenario 'visit users url' do
+      user.admin = false
+      user.save
+
+      visit users_path
+      expect(page).to have_content('not authorized')
+    end
+
+    scenario 'visit users profile' do
+      user.admin = false
+      user.save
+
+      visit user_path(:id => user.id)
+      expect(page).to have_content('not authorized')
+    end
+  end
+
+  describe 'as admin user' do
+    before(:each) do
+      user.admin = true
+      user.save
+
+      sign_in(user.email, user.password)
+    end
+
+    scenario 'visit users url' do
+      visit users_path
+      expect(page).to have_content(user.first_name)
+    end
+
+    scenario 'visit users profile' do
+      visit user_path(:id => user.id)
+      expect(page).to have_content(user.first_name)
+    end
+  end
+
+  def sign_in(email, password)
+    visit new_user_session_path
+
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_button 'Sign in'
+  end
+
+  def sign_out(user)
+    sign_in(user.email, user.password)
+    click_link 'Logout'
+  end
+end
