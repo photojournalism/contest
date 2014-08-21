25ee490dc894388cb1550e6e9fc4e11eae1027f5
Casey Scarborough <caseyscarborough@gmail.com>
2014-08-21 13:25:19 -0400

    Styling updates and display messages on homepage for contest end and close date.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 
   def index
+    @contest = Contest.current
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe HomeController, :type => :controller do
   describe "GET index" do
+    let(:contest) { FactoryGirl.create(:contest) }
+
     it "renders the index template" do
       get :index
       expect(response).to render_template(:index)
+    end
+
+    it 'assigns the contest' do
+      contest.save
+      get :index
+      expect(assigns(:contest)).to eq(contest)
     end
   end
 end
