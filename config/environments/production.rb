1bdebf84396dcf50edd341ec4fcca675a63d8836
Casey Scarborough <caseyscarborough@gmail.com>
2014-09-01 17:06:23 -0400

    Update host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'contest.atlantaseminar.org' }
+  config.action_mailer.default_url_options = { host: 'contest.photojournalism.org' }
 
 end
