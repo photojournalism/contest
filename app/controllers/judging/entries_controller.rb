00b123ac5dbc7e720c18fea5855d910bbec15aef
Casey Scarborough <caseyscarborough@gmail.com>
2014-10-11 11:12:26 -0400

    Convert tabs to spaces in Judging::EntriesController.

--- app/controllers/judging/entries_controller.rb
+++ app/controllers/judging/entries_controller.rb
@@ -3,9 +3,9 @@ class Judging::EntriesController < ApplicationController
   layout "judging"
 
   def index
-  	@contest = Contest.current
-  	@categories = @contest.categories
-  	@current_category = params[:category_id] ? Category.find(params[:category_id]) : @categories.first
-  	@entries = Entry.where(:contest => @contest, :category => @current_category)
+    @contest = Contest.current
+    @categories = @contest.categories
+    @current_category = params[:category_id] ? Category.find(params[:category_id]) : @categories.first
+    @entries = Entry.where(:contest => @contest, :category => @current_category)
   end
 end
