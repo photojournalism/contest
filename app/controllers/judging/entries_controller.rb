4b5e3c31c1f4e826499a82a07b22ae9c001dc1f1
Casey Scarborough <caseyscarborough@gmail.com>
2014-10-11 14:56:32 -0400

    Extract duplicate functionality into private method and create Judging::Entries#show action.

--- app/controllers/judging/entries_controller.rb
+++ app/controllers/judging/entries_controller.rb
@@ -3,9 +3,20 @@ class Judging::EntriesController < ApplicationController
   layout "judging"
 
   def index
+    get_shared_fields
+  end
+
+  def show
+    @entry = Entry.where(:unique_hash => params[:hash]).first
+    get_shared_fields(@entry.category)
+  end
+
+  private
+
+  def get_shared_fields(category=nil)
     @contest = Contest.current
     @categories = @contest.categories
-    @current_category = params[:category_id] ? Category.find(params[:category_id]) : @categories.first
+    @current_category = category ? category : (params[:category_id] ? Category.find(params[:category_id]) : @categories.first)
     @entries = Entry.where(:contest => @contest, :category => @current_category)
   end
 end
