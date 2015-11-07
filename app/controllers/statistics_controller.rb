a698a954f3b689662c47468468587bc74c3b00c8
Casey Scarborough <caseyscarborough@gmail.com>
2015-11-07 14:18:59 -0500

    Add category counts to statistics page

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -7,12 +7,17 @@ class StatisticsController < ApplicationController
 
     @number_of_entries = @contest.entries.size
     @number_of_images = 0
+    @category_counts = {}
     users = []
 
     @contest.entries.each do |entry|
       if !entry.pending
         users << entry.user.name
         @number_of_images += entry.images.length
+        if (!@category_counts[entry.category.name])
+          @category_counts[entry.category.name] = 0
+        end
+        @category_counts[entry.category.name] += 1
       end
     end
 
